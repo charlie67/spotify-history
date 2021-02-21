dd2f9f9e84cf56ba871e6c78244b0f6a045d32ee
Charlie <charlie@charlie.to>
2021-02-21 07:20:50 +0000

    Remove jenkins

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
 ./gradlew jar
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:${BRANCH_NAME}-server --push .
+docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:${BRANCH_NAME}-ui --push -f Dockerfile-ui .
