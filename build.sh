fc7599c364641c6a29b8ef40ebc5f4fdd4a6b817
Charlie <charlie@charlie.to>
2021-02-21 08:58:41 +0000

    rename branch

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ./gradlew jar
-echo $$2 | docker login -u $1 --password-stdin
+echo $2 | docker login -u $1 --password-stdin
 sudo docker build --tag $1/spotify-wrapped-server:${GITHUB_REF##*/} -f Dockerfile-server .
 sudo docker build --tag $1/spotify-wrapped-ui:${GITHUB_REF##*/} -f Dockerfile-ui .
 sudo docker push $1/spotify-wrapped-server:${GITHUB_REF##*/}
