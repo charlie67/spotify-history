ffb960286525a63ce86c4fae684e1eadb14ea66d
Charlie <charlie@charlie.to>
2021-02-21 07:23:21 +0000

    Update branch name

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ./gradlew jar
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:${BRANCH_NAME}-server --push .
-docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:${BRANCH_NAME}-ui --push -f Dockerfile-ui .
+docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:${CIRCLE_BRANCH}-server --push .
+docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:${CIRCLE_BRANCH}-ui --push -f Dockerfile-ui .
