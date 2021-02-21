3e4657e094554eaa0c3c316df524c3bc60029b19
Charlie <charlie@charlie.to>
2021-02-21 09:25:22 +0000

    Sort out docker creation

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-./gradlew jar
+./gradlew bootJar
 echo $2 | docker login -u $1 --password-stdin
-sudo docker build --tag $1/spotify-wrapped-server:${GITHUB_REF##*/} -f Dockerfile-server .
-sudo docker build --tag $1/spotify-wrapped-ui:${GITHUB_REF##*/} -f Dockerfile-ui .
-sudo docker push $1/spotify-wrapped-server:${GITHUB_REF##*/}
-sudo docker push $1/spotify-wrapped-ui:${GITHUB_REF##*/}
+sudo docker build --tag $1/spotify-history-server:${GITHUB_REF##*/} -f Dockerfile-server .
+sudo docker build --tag $1/spotify-history-ui:${GITHUB_REF##*/} -f Dockerfile-ui .
+sudo docker push $1/spotify-history-server:${GITHUB_REF##*/}
+sudo docker push $1/spotify-history-ui:${GITHUB_REF##*/}
