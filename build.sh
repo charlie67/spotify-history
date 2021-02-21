0f6ac475e4f37b76ff33bd57afcc0588280a68e3
Charlie <charlie@charlie.to>
2021-02-21 08:04:06 +0000

    Change to docker build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./gradlew jar
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build --tag $DOCKER_USERNAME/spotify-wrapped-server:$CIRCLE_BRANCH -f Dockerfile- .
+docker build --tag $DOCKER_USERNAME/spotify-wrapped-server:$CIRCLE_BRANCH -f Dockerfile-server .
 docker build --tag $DOCKER_USERNAME/spotify-wrapped-ui:$CIRCLE_BRANCH -f Dockerfile-ui .
 docker push $DOCKER_USERNAME/spotify-wrapped-server:$CIRCLE_BRANCH
 docker push $DOCKER_USERNAME/spotify-wrapped-ui:$CIRCLE_BRANCH
