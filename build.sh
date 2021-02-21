e041e3316ce890f3816fe0918336ffc4d40e6168
Charlie <charlie@charlie.to>
2021-02-21 08:41:18 +0000

    rename branch

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./gradlew jar
-echo ${DOCKER_PASSWORD} | docker login -u ${DOCKER_USERNAME} --password-stdin
-sudo docker build --tag ${DOCKER_PASSWORD}/spotify-wrapped-server:${GITHUB_REF##*/} -f Dockerfile-server .
-sudo docker build --tag ${DOCKER_PASSWORD}/spotify-wrapped-ui:${GITHUB_REF##*/} -f Dockerfile-ui .
-sudo docker push ${DOCKER_PASSWORD}/spotify-wrapped-server:${GITHUB_REF##*/}
-sudo docker push ${DOCKER_PASSWORD}/spotify-wrapped-ui:${GITHUB_REF##*/}
+echo ${docker.DOCKER_PASSWORD} | docker login -u ${docker.DOCKER_USERNAME} --password-stdin
+sudo docker build --tag ${docker.DOCKER_PASSWORD}/spotify-wrapped-server:${GITHUB_REF##*/} -f Dockerfile-server .
+sudo docker build --tag ${docker.DOCKER_PASSWORD}/spotify-wrapped-ui:${GITHUB_REF##*/} -f Dockerfile-ui .
+sudo docker push ${docker.DOCKER_PASSWORD}/spotify-wrapped-server:${GITHUB_REF##*/}
+sudo docker push ${docker.DOCKER_PASSWORD}/spotify-wrapped-ui:${GITHUB_REF##*/}
