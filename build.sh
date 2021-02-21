985743d20315c9ce3431eb4aa937631da06f7e21
Charlie <charlie@charlie.to>
2021-02-21 09:34:14 +0000

    remove sudo

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ./gradlew bootJar
 echo $2 | docker login -u $1 --password-stdin
-sudo docker build --tag $1/spotify-history-server:${GITHUB_REF##*/} -f Dockerfile-server .
-sudo docker build --tag $1/spotify-history-ui:${GITHUB_REF##*/} -f Dockerfile-ui .
-sudo docker push $1/spotify-history-server:${GITHUB_REF##*/}
-sudo docker push $1/spotify-history-ui:${GITHUB_REF##*/}
+docker build --tag $1/spotify-history-server:${GITHUB_REF##*/} -f Dockerfile-server .
+docker build --tag $1/spotify-history-ui:${GITHUB_REF##*/} -f Dockerfile-ui .
+docker push $1/spotify-history-server:${GITHUB_REF##*/}
+docker push $1/spotify-history-ui:${GITHUB_REF##*/}
