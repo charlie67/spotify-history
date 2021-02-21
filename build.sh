88e105c2a271f06e4145f71cbfea779ff44f37d9
Charlie <charlie@charlie.to>
2021-02-21 07:42:15 +0000

    Change to docker build

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 ./gradlew jar
-echo "Logging into docker"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-echo "Logged into docker"
-docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:${CIRCLE_BRANCH}-server --push .
-docker buildx build --platform linux/amd64 --tag $DOCKER_USERNAME/discord-music-bot:${CIRCLE_BRANCH}-ui --push -f Dockerfile-ui .
+docker build --tag $DOCKER_USERNAME/discord-music-bot:${CIRCLE_BRANCH}-server --push .
+docker build --tag $DOCKER_USERNAME/discord-music-bot:${CIRCLE_BRANCH}-ui --push -f Dockerfile-ui .
