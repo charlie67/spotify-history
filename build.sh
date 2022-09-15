69cacfbbc98cc1b3709cc71f61df72d4a0f93ef4
Charlie <charlierobinson080@gmail.com>
2022-09-15 19:39:48 +0000

    Create playlist from endpoint

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 ./gradlew bootJar
 echo $2 | docker login -u $1 --password-stdin
-docker build --tag $1/spotify-history:${GITHUB_REF_NAME} -f Dockerfile-server .
+docker build --tag $1/spotify-history:${GITHUB_REF_NAME} -f Dockerfile .
 docker push $1/spotify-history:${GITHUB_REF_NAME}
