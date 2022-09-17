9dcb4a5cba29ee918d3cd19bf965a3aac21592df
Charlie <charlierobinson080@gmail.com>
2022-09-17 15:16:11 +0000

    Maven update

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-./gradlew bootJar
+mvnw clean install
 echo $2 | docker login -u $1 --password-stdin
 docker build --tag $1/spotify-history:${GITHUB_REF_NAME} -f Dockerfile .
 docker push $1/spotify-history:${GITHUB_REF_NAME}
