6b0199fbdfc164e65e4fe1ee7385db20c8c8751f
Charlie <charlierobinson080@gmail.com>
2022-09-19 14:22:51 +0000

    flyway and maven upgrade

--- src/main/java/to/charlie/spotifyplayhistory/domain/repository/ArtistRepository.java
+++ src/main/java/to/charlie/spotifyplayhistory/domain/repository/ArtistRepository.java
@@ -3,12 +3,12 @@ package to.charlie.spotifyplayhistory.domain.repository;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import to.charlie.spotifyplayhistory.domain.entity.Artist;
 
 
-@RepositoryRestResource
+@Repository
 public interface ArtistRepository extends CrudRepository<Artist, Integer>
 {
   Optional<Artist> findByArtistId(String artistId);
